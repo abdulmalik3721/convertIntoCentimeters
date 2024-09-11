f89351a081711a1977cf184cd9b4f51b57e75725
ZAMIUL HAQ <ZamilulHaq.ksg@gmail.com>
2024-09-11 11:02:19 +0530

    added new method

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("5fe , 8inch = " + convertIntoCentimeters(5,8) + "cm");
+        System.out.println("68inch = " + convertIntoCentimeters(68) + "cm");
+        convertIntoCentimeters();
+
+    }
+
+    public static double convertIntoCentimeters(int inches){
+        return inches*2.54;
+    }
+
+    public static double convertIntoCentimeters(int feet, int inches){
+        return ((feet*12)+ inches)*2.54;
+    }
+    public static void convertIntoCentimeters(){
+        System.out.println("change the mood of your life");
+    }
+
+}
